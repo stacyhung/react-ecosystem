a4fa383256ebcdc58684aacf007a2b3f3c22cde0
Stacy Hung <sshung@gmail.com>
2020-12-18 15:54:19 -0800

    add local storage for todo items

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { createStore, combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to local storage on the web
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'; // tells ReduxPersist how deep to go
 import { todos } from './todos/reducers';
 
 const reducers = {
     todos,
 };
 
-const rootReducer = combineReducers(reducers); // for createStore function
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+}
 
-export const configureStore = () => createStore(rootReducer);
+const rootReducer = combineReducers(reducers); // for createStore function
+const persistedReducer = persistReducer(persistConfig, rootReducer); // persistConfig tells us where to save and how to store app data
+
+export const configureStore = () => createStore(persistedReducer);
